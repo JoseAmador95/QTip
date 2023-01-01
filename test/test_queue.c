ccf530fc7f219846b59bc472225fc0975b542d33
Jose Amador Demeneghi <jose_amador95@hotmail.com>
2023-01-01 21:56:46 +0000

    Add unit test file

---
+++ test/test_queue.c
@@ -0,0 +1,96 @@
+#include "queue.h"
+#include "unity.h"
+
+#include <string.h>
+
+#define QUEUE_SIZE 100U
+
+typedef uint8_t type_t;
+
+queueContext_t context;
+type_t *queue[QUEUE_SIZE];
+type_t *buffer[QUEUE_SIZE];
+
+void setUp(void)
+{
+    queue_init(&context, queue, QUEUE_SIZE, sizeof(type_t *));
+}
+
+void tearDown(void)
+{
+    memset(queue, 0U, sizeof(queue));
+}
+
+void test_put_get(void)
+{
+    type_t element1 = 1U;
+    type_t element2 = 2U;
+    type_t buff = 0U;
+    size_t size = 0U;
+    TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element1));
+    TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element2));
+    TEST_ASSERT(QUEUE_GOOD == queue_pop(&context, &buff));
+    TEST_ASSERT_EQUAL_UINT8(element1, buff);
+    TEST_ASSERT(QUEUE_GOOD == queue_pop(&context, &buff));
+    TEST_ASSERT_EQUAL_UINT8(element2, buff);
+}
+
+void test_peek(void)
+{
+    type_t element1 = 1U;
+    type_t element2 = 2U;
+    size_t size = 0U;
+    TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element1));
+    TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element2));
+    queue_peek(&context, (void **)buffer, &size);
+    TEST_ASSERT_EQUAL_size_t(2U, size);
+    TEST_ASSERT_EQUAL_UINT8(element1, *buffer[0U]);
+    TEST_ASSERT_EQUAL_UINT8(element2, *buffer[1U]);
+}
+
+void test_rollover(void)
+{
+    type_t element = 0U;
+
+    for (int i = 0U; i < QUEUE_SIZE - 2U; i++)
+    {
+        element = i;
+        TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element));
+    }
+
+    for (int i = 0U; i < 4U; i++)
+    {
+        TEST_ASSERT(QUEUE_GOOD == queue_pop(&context, &element));
+    }
+
+    for (int i = 0U; i < 6U; i++)
+    {
+        element = i;
+        TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element));
+    }
+    TEST_ASSERT(QUEUE_FULL == queue_put(&context, &element));
+}
+
+void test_empty(void)
+{
+    type_t element = 0U;
+    TEST_ASSERT(QUEUE_EMPTY == queue_pop(&context, &element));
+}
+
+void test_stress(void)
+{
+    type_t element = 0U;
+
+    for (int i = 0U; i < QUEUE_SIZE; i++)
+    {
+        element = i;
+        TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element));
+    }
+    TEST_ASSERT(QUEUE_FULL == queue_put(&context, &element));
+
+    for (int i = 0U; i < QUEUE_SIZE; i++)
+    {
+        TEST_ASSERT(QUEUE_GOOD == queue_pop(&context, &element));
+    }
+    TEST_ASSERT(QUEUE_EMPTY == queue_pop(&context, &element));
+}
