40cf76a24f1f26dd0a8bb7500c62b42f46418349
Jose Amador Demeneghi <jose_amador95@hotmail.com>
2023-01-01 23:32:05 +0000

    Change unit test

--- test/test_queue.c
+++ test/test_queue.c
@@ -8,12 +8,12 @@
 typedef uint8_t type_t;
 
 queueContext_t context;
-type_t *queue[QUEUE_SIZE];
-type_t *buffer[QUEUE_SIZE];
+type_t queue[QUEUE_SIZE];
+type_t buffer[QUEUE_SIZE];
 
 void setUp(void)
 {
-    queue_init(&context, queue, QUEUE_SIZE, sizeof(type_t *));
+    queue_init(&context, queue, QUEUE_SIZE, sizeof(type_t));
 }
 
 void tearDown(void)
@@ -21,12 +21,12 @@ void tearDown(void)
     memset(queue, 0U, sizeof(queue));
 }
 
-void test_put_get(void)
+void test_put_pop(void)
 {
     type_t element1 = 1U;
     type_t element2 = 2U;
-    type_t buff = 0U;
-    size_t size = 0U;
+    type_t buff     = 0U;
+    size_t size     = 0U;
     TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element1));
     TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element2));
     TEST_ASSERT(QUEUE_GOOD == queue_pop(&context, &buff));
@@ -39,31 +39,35 @@ void test_peek(void)
 {
     type_t element1 = 1U;
     type_t element2 = 2U;
-    size_t size = 0U;
+    size_t size     = 0U;
     TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element1));
     TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element2));
-    queue_peek(&context, (void **)buffer, &size);
+    queue_peek(&context, buffer, &size);
     TEST_ASSERT_EQUAL_size_t(2U, size);
-    TEST_ASSERT_EQUAL_UINT8(element1, *buffer[0U]);
-    TEST_ASSERT_EQUAL_UINT8(element2, *buffer[1U]);
+    TEST_ASSERT_EQUAL_UINT8(element1, buffer[0U]);
+    TEST_ASSERT_EQUAL_UINT8(element2, buffer[1U]);
 }
 
 void test_rollover(void)
 {
     type_t element = 0U;
 
-    for (int i = 0U; i < QUEUE_SIZE - 2U; i++)
+    const size_t firstPut  = QUEUE_SIZE - 2U;
+    const size_t fisrtPop  = 4U;
+    const size_t secondPut = 6U;
+
+    for (int i = 0U; i < firstPut; i++)
     {
         element = i;
         TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element));
     }
 
-    for (int i = 0U; i < 4U; i++)
+    for (int i = 0U; i < fisrtPop; i++)
     {
         TEST_ASSERT(QUEUE_GOOD == queue_pop(&context, &element));
     }
 
-    for (int i = 0U; i < 6U; i++)
+    for (int i = 0U; i < secondPut; i++)
     {
         element = i;
         TEST_ASSERT(QUEUE_GOOD == queue_put(&context, &element));
