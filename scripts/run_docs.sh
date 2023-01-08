50520a7c0957b7783e18d2883703ca831c55bc8b
Jose Amador Demeneghi <jose_amador95@hotmail.com>
2023-01-08 17:15:33 +0000

    add docs to the CI

---
+++ scripts/run_docs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+ARTIFACTS="artifacts"
+NAME="docs"
+OUTDIR="$ARTIFACTS/$NAME"
+RESULT_FILE="$ARTIFACTS/results.txt"
+LOG="$OUTDIR/$NAME.log"
+DOCS="build/doxygen"
+
+mkdir -p $OUTDIR
+
+COMMAND="doxygen"
+
+CONFIG_FILE="docs/Doxyfile"
+
+$COMMAND $CONFIG_FILE &> $LOG
+RET=$?
+
+echo "$COMMAND $CONFIG_FILE"
+
+cat $LOG
+
+echo "${NAME^^}=$RET" >> $RESULT_FILE
+
+cp -r $DOCS/* $OUTDIR
+
+exit 0
