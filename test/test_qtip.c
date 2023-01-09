7d938b6ad841896f8f1ce3ab4fcb666b5a61c006
Jose Amador Demeneghi <jose_amador95@hotmail.com>
2023-01-09 21:13:41 +0000

    More unit tests

--- test/test_qtip.c
+++ test/test_qtip.c
@@ -10,7 +10,8 @@
 
 #include <string.h>
 
-#define QUEUE_SIZE 10U
+#define QTIP_ASSERT_OK(exp) TEST_ASSERT(QTIP_STATUS_OK == (exp))
+#define QUEUE_SIZE          10U
 
 typedef uint32_t type_t;
 
@@ -104,4 +105,95 @@ void test_stress(void)
         TEST_ASSERT(QTIP_STATUS_OK == qtip_pop(&context, &element));
     }
     TEST_ASSERT(QTIP_STATUS_EMPTY == qtip_pop(&context, &element));
+}
+
+void test_purge(void)
+{
+    type_t item = QUEUE_SIZE;
+    size_t qty  = 0U;
+
+    for (int i = 0U; i < item; i++)
+    {
+        TEST_ASSERT(QTIP_STATUS_OK == qtip_put(&context, &item));
+    }
+
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_purge(&context));
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_count_items(&context, &qty));
+    TEST_ASSERT_EQUAL_size_t(0U, qty);
+}
+
+void test_get_front_rear(void)
+{
+    const type_t itemFront = 1U;
+    const type_t itemRear  = 10U;
+    type_t item            = 0U;
+
+    for (int i = itemFront; i <= itemRear; i++)
+    {
+        TEST_ASSERT(QTIP_STATUS_OK == qtip_put(&context, &i));
+    }
+
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_get_front(&context, &item));
+    TEST_ASSERT_EQUAL_UINT32(itemFront, item);
+    item = 0U;
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_get_rear(&context, &item));
+    TEST_ASSERT_EQUAL_UINT32(itemRear, item);
+}
+
+void test_lock(void)
+{
+    type_t item = 0;
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_lock(&context));
+    TEST_ASSERT(QTIP_STATUS_LOCKED == qtip_is_locked(&context));
+    TEST_ASSERT(QTIP_STATUS_LOCKED == qtip_put(&context, &item));
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_unlock(&context));
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_put(&context, &item));
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_is_locked(&context));
+}
+
+void test_telemetry(void)
+{
+    type_t item      = 0U;
+    size_t processed = 0U;
+    size_t total     = 0U;
+
+    for (int i = 0; i < QUEUE_SIZE; i++)
+    {
+        TEST_ASSERT(QTIP_STATUS_OK == qtip_put(&context, &item));
+    }
+
+    for (int i = 0; i < QUEUE_SIZE; i++)
+    {
+        TEST_ASSERT(QTIP_STATUS_OK == qtip_pop(&context, &item));
+    }
+
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_total_processed_items(&context, &processed));
+    TEST_ASSERT(QTIP_STATUS_OK == qtip_total_enqueued_items(&context, &total));
+    TEST_ASSERT_EQUAL_size_t(QUEUE_SIZE, total);
+    TEST_ASSERT_EQUAL_size_t(QUEUE_SIZE, processed);
+}
+
+void test_null_ptr(void) // NOLINT
+{
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_init(NULL, NULL, NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_put(NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_pop(NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_peek(NULL, NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_purge(NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_get_front(NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_get_rear(NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_is_full(NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_is_empty(NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_count_items(NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_is_locked(NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_lock(NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_unlock(NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_total_enqueued_items(NULL, NULL));
+    TEST_ASSERT(QTIP_STATUS_NULL_PTR == qtip_total_processed_items(NULL, NULL));
+}
+
+void test_invalid_size(void)
+{
+    TEST_ASSERT(QTIP_STATUS_INVALID_SIZE == qtip_init(&context, queue, 0U, sizeof(type_t)));
+    TEST_ASSERT(QTIP_STATUS_INVALID_SIZE == qtip_init(&context, queue, QUEUE_SIZE, 0U));
 }
